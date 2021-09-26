387557153c7a72ba760a2efbf93f9c13d185924c
dulovici <igorx24@gmail.com>
2021-09-26 15:52:50 +0200

    v1.0

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 
+(function () {
+'use strict'
 
-console.log('dev')
+
+const input = document.querySelector('.input-wr input');
+const button = document.querySelector('.input-wr button');
+const errorMsg = document.querySelector('.error');
+
+
+function checkEmail() {
+    var re = /\S+@\S+\.\S+/;
+    re.test(input.value) 
+    ? errorMsg.classList.add('throw') 
+    : errorMsg.classList.remove('throw')
+}
+
+
+button.addEventListener('click', checkEmail)
+
+})();
+
+
+
+
