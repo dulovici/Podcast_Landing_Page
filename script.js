7bd7bd37ff111df1956afea4d837c379ffa21c14
dulovici <igorx24@gmail.com>
2021-09-26 16:04:30 +0200

    code clean-up

--- script.js
+++ script.js
@@ -2,19 +2,17 @@
 (function () {
 'use strict'
 
-
 const input = document.querySelector('.input-wr input');
 const button = document.querySelector('.input-wr button');
 const errorMsg = document.querySelector('.error');
 
 
 function checkEmail() {
-    var re = /\S+@\S+\.\S+/;
+    const re = /\S+@\S+\.\S+/;
     re.test(input.value) 
     ? errorMsg.classList.add('throw') 
     : errorMsg.classList.remove('throw')
 }
-
 
 button.addEventListener('click', checkEmail)
 
