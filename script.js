e8c7781fd67f711a8ca34d35e67e95640d46cc01
dulovici <81692156+dulovici@users.noreply.github.com>
2021-09-30 10:22:27 +0200

    Dev (#1)
    * dev init
    
    * dev starter
    
    * initial style
    
    * desktop version done
    
    * raw layout done
    
    * v1.0
    
    * code clean-up
    
    * input outline removed
    
    * mobile and lanscape corection
    
    * input width
    
    * code cleanup - small style changes
    
    * v1.1
    
    * button:hover, icons-size
    
    * mobile imgs corection
    
    * logos correction
    
    * v2.0

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 
+(function () {
+    'use strict'
+    
+    const input = document.querySelector('.input-wr input');
+    const button = document.querySelector('.input-wr button');
+    const errorMsg = document.querySelector('.error');
+    
+    
+    function checkEmail() {
+        const re = /\S+@\S+\.\S+/;
+        re.test(input.value) 
+        ? errorMsg.classList.add('throw') 
+        : errorMsg.classList.remove('throw')
 
-console.log('main')
+        console.log('ff')
+    }
+    
+    button.addEventListener('click', checkEmail)
+    
+
+    })();
+
+
+  
